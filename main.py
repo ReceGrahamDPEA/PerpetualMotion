a8789c86154035dcb0c521173e2f77313f002159
Rece Graham <recegraham@dpengineering.org>
2022-12-13 12:07:22 -0800

    Added documentation of most if not all functions

--- main.py
+++ main.py
@@ -81,6 +81,8 @@ class Functions:
 
     def panic(self):
 
+        """Does everything the same as the quit function but more aggressively."""
+
         ramp.softStop()
         cyprus.set_servo_position(2, self.servo_closed)
         sleep(.1)
@@ -120,6 +122,9 @@ class MainScreen(Screen, Functions):
     ramp_sens_upper_state = 2
 
     def __init__(self, **kwargs):
+
+        """Things that happen at the initializing of the Main Screen"""
+
         super(MainScreen, self).__init__(**kwargs)
         self.initialize()
 
@@ -128,6 +133,9 @@ class MainScreen(Screen, Functions):
         cyprus.set_pwm_values(1, period_value=100000, compare_value=0, compare_mode=cyprus.LESS_THAN_OR_EQUAL)
 
     def variables(self, dt):
+
+        """Creating variables with only one clock function. This should take less processing power than running
+        a million clocks"""
 
         if cyprus.read_gpio() & 0b0010:  # binary bitwise AND of the value returned from read.gpio()
             self.ramp_sens_lower_state = 0
@@ -151,6 +159,8 @@ class MainScreen(Screen, Functions):
 
     def toggleGate(self):
 
+        """Opens and closes the gate. Uses a "gate_pos" variable to find out the gate's state"""
+
         if self.gate_pos == 0:
             cyprus.set_servo_position(2, self.servo_open)
             self.gate_pos = 1
@@ -161,6 +171,8 @@ class MainScreen(Screen, Functions):
 
     def toggleStaircase(self):
 
+        """Toggles the staircase on and off. The PMW value is controlled by the Kivy UI"""
+
         if self.stair_state == 0:
             cyprus.set_pwm_values(1, period_value=100000, compare_value=self.ids.staircaseSpeed.value, compare_mode=cyprus.LESS_THAN_OR_EQUAL)
             self.stair_state = 1
@@ -170,6 +182,8 @@ class MainScreen(Screen, Functions):
             self.stair_state = 0
 
     def toggleRamp(self):
+
+        """Function to toggle the ramp. Only works if the ball is at the lower sensor and the state is 1"""
 
         if self.ramp_sens_lower_state == 1:
             ramp.start_relative_move(28.5)
@@ -184,6 +198,9 @@ class MainScreen(Screen, Functions):
             print("ramp is going down!")
 
     def auto(self):
+
+        """Goes through the machine automatically. Will only work if the ball and ramp are at the lower position"""
+
         print("Run through one cycle of the perpetual motion machine")
 
         if self.ramp_sens_lower_state == 1:
@@ -213,6 +230,9 @@ class MainScreen(Screen, Functions):
 
     def setRampSpeed(self, speed):
 
+        """Sets the ramp speed. The speed variable in the function definition is not used as it wasn't necessary
+        but was included in the example."""
+
         if not ramp.is_busy():
             ramp.set_speed(self.ids.rampSpeed.value * .02)
             ramp_check = self.ids.rampSpeed.value * .02
@@ -224,11 +244,16 @@ class MainScreen(Screen, Functions):
 
     def setStaircaseSpeed(self, speed):
 
+        """Updates the staircase speed only if it is on."""
+
         if self.stair_state == 1:
 
             cyprus.set_pwm_values(1, period_value=100000, compare_value=self.ids.staircaseSpeed.value, compare_mode=cyprus.LESS_THAN_OR_EQUAL)
 
     def initialize(self):
+
+        """Tells the ramp to go home. This doesn't usually work as the ramp needs to be at home when turning it on
+        and if it is not at home when the program is initialized then it will not go home and needs to be reset"""
 
         ramp.goHome()
 
@@ -242,6 +267,9 @@ class MainScreen(Screen, Functions):
 
     def quit(self):
 
+        """Quit function. This is supposed to put all the components back to their "natural state" but half the time
+        it doesn't actually work and the servo does what it wants."""
+
         ramp.free_all()
         sleep(.5)
         cyprus.set_servo_position(2, .25)
@@ -253,6 +281,9 @@ class MainScreen(Screen, Functions):
         MyApp().stop()
 
     def PANIC(self):
+
+        """Calls the panic function"""
+
         self.panic()
 
 
